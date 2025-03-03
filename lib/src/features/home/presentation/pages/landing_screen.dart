5c56802bb8e73be50010875872371004bfbcd2a6
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-03 12:57:25 +0530

    Landing Screen implementation

--- lib/src/features/home/presentation/pages/landing_screen.dart
+++ lib/src/features/home/presentation/pages/landing_screen.dart
@@ -5,6 +5,18 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/money_monastery.png'),
+          const SizedBox(height: 10,),
+          Text('Your Gateway to Financial Nirvana!',
+          style: Theme.of(context).textTheme.titleSmall,),
+
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
