2a823ae40b050b0cadacc21607ab33104f77ace2
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-03 15:03:06 +0530

    Landing screen completed

--- lib/src/features/home/presentation/pages/landing_screen.dart
+++ lib/src/features/home/presentation/pages/landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:money_monastery/src/features/home/presentation/widgets/custom_button.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
@@ -6,16 +7,36 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/money_monastery.png'),
-          const SizedBox(height: 10,),
-          Text('Your Gateway to Financial Nirvana!',
-          style: Theme.of(context).textTheme.titleSmall,),
-
-          const Spacer(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/money_monastery.png'),
+                    const SizedBox(height: 10,),
+                    Text('Your Gateway to Financial Nirvana!',
+                    style: Theme.of(context).textTheme.titleSmall,),
+                  ],
+                ),
+              ),
+          
+              //const Spacer(),
+              CustomButton(title: 'Sign up', onPressed: () {},
+              backgroundColor: Colors.black,
+              textColor: Colors.white,),
+              const SizedBox(height: 15,),
+              CustomButton(title: 'Login', onPressed: () {}),
+              const SizedBox(height: 50,)
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/src/features/home/presentation/widgets/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.backgroundColor,
+    this.borderRadius,
+    this.padding,
+    this.textColor
+    });
+
+  final VoidCallback onPressed;
+  final String title;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final EdgeInsets? padding;
+  final double? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      //height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed, 
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor ?? Colors.black.withOpacity(0.1),
+          padding: EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0)
+          )
+        ),
+        child: Text(title,
+        style: Theme.of(context).textTheme.titleMedium!.copyWith(color: textColor
+        )),
+      ),
+    );
+  }
+}
